6c82997185bcd9f37693b8f9b89ce6a007ed8e6e
joshc <joshcoyle2002@gmail.com>
2025-11-16 01:11:01 +0000

    phone optimisation pt 3

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent').lower()
+    return render_template('index.html')
 
-    if 'mobile' in user_agent or 'android' in user_agent or 'iphone' in user_agent:
-        return render_template('index-mobile.html')
-    else:
-        return render_template('index.html')
 @app.route('/about')
 def about_me():
     return render_template('aboutme.html')
